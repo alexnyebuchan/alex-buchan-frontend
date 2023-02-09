fe7005b63d297e5708e77bab65d485d11bc5d976
Alex <alexnyebuchan@gmail.com>
2023-02-09 12:44:03 +0000

    created Links for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,21 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import github from '../public/images/icons/github.svg';
+
 function Navbar() {
-  return <div>AB | Contact | About | Github </div>;
+  return (
+    <div>
+      <div>AB</div>
+      <div>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="https://github.com/alexnyebuchan">
+          <Image alt="/" src={github} width={25} height={25} />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
