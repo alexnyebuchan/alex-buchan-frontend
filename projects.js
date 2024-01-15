3285f01ed156cbdf094d1cfd94527f5f5b1b34a7
alexnyebuchan <alexnyebuchan@gmail.com>
2024-01-15 12:52:06 +0000

    changed order of projects

--- projects.js
+++ projects.js
@@ -53,6 +53,50 @@ const projects = [
   },
   {
     id: 3,
+    title: 'Al Zanders Current Website',
+    description:
+      'Full stack application displaying latest news and releases for side hustle music production and DJ business. Utilises a Django backend and React frontend. Integrated React-Bootstrap component library for consistent styles and fast development.',
+    github: 'https://github.com/alexnyebuchan/alzandersone',
+    url: 'https://www.alzanders.co.uk/',
+    tech: [
+      {
+        url: '/images/icons/react.svg',
+        title: 'React.js',
+      },
+      {
+        url: '/images/icons/bootstrap.svg',
+        title: 'Bootstrap',
+      },
+      {
+        url: '/images/icons/django-icon.svg',
+        title: 'Django',
+      },
+    ],
+    image: '/images/project/alzanders.jpg',
+    popupImage: '/images/project/alzanders-main.jpg',
+  },
+  {
+    id: 4,
+    title: 'Guardian Clone',
+    description:
+      'A clone of the Guardian news website (Technology Section) to demonstrate ability to translate user designs into a functional website. Utilises the Guardian news REST Api to provide real time news information. Translated designs from the real website into Next.js code. Designed with SEO best practices in mind.',
+    github: 'https://github.com/alexnyebuchan/guardian-frontend',
+    url: 'https://guardian-frontend.vercel.app/',
+    tech: [
+      {
+        url: '/images/icons/nextjs-icon.svg',
+        title: 'Next.js',
+      },
+      {
+        url: '/images/icons/sass-1.svg',
+        title: 'Sass',
+      },
+    ],
+    image: '/images/project/guardian image.png',
+    popupImage: '/images/project/guardianpopup.png',
+  },
+  {
+    id: 5,
     title: 'Web Dev Fundamentals',
     description:
       'This simple web application takes a key web development term such as ‘URL’ or ‘DOM’ from a list of terms and returns one at random, with a title and definition. The list is in JSON and updated regularly, the app is configured with Vite, Typescript and React. Currently undergoing Jest integration on a seperate Git Branch. Download from here or use online.',
@@ -79,28 +123,9 @@ const projects = [
     image: '/images/project/webdevfund.jpg',
     popupImage: '/images/project/webdevfund-main.jpg',
   },
+  
   {
-    id: 4,
-    title: 'Guardian Clone',
-    description:
-      'A clone of the Guardian news website (Technology Section) to demonstrate ability to translate user designs into a functional website. Utilises the Guardian news REST Api to provide real time news information. Translated designs from the real website into Next.js code. Designed with SEO best practices in mind.',
-    github: 'https://github.com/alexnyebuchan/guardian-frontend',
-    url: 'https://guardian-frontend.vercel.app/',
-    tech: [
-      {
-        url: '/images/icons/nextjs-icon.svg',
-        title: 'Next.js',
-      },
-      {
-        url: '/images/icons/sass-1.svg',
-        title: 'Sass',
-      },
-    ],
-    image: '/images/project/guardian image.png',
-    popupImage: '/images/project/guardianpopup.png',
-  },
-  {
-    id: 5,
+    id: 6,
     title: 'CS50: Recipe Maker',
     description:
       "CS50 Final Project which I passed. The Recipe Maker is a full-stack web application that takes as input the user’s perishable and non-perishable kitchen ingredients before making a fetch request to the Chat-GPT API to get some suggested recipes. The frontend is a simple mixture of HTML, CSS, and vanilla JavaScript. The backend utilizes Flask, a database, and SQL to store a user's larder so they don’t have to retype it every time they log in.",
@@ -126,30 +151,6 @@ const projects = [
     ],
     image: '/images/project/cs50.jpg',
     popupImage: '/images/project/cs50-main.jpg',
-  },
-  {
-    id: 6,
-    title: 'Al Zanders Current Website',
-    description:
-      'Full stack application displaying latest news and releases for side hustle music production and DJ business. Utilises a Django backend and React frontend. Integrated React-Bootstrap component library for consistent styles and fast development.',
-    github: 'https://github.com/alexnyebuchan/alzandersone',
-    url: 'https://www.alzanders.co.uk/',
-    tech: [
-      {
-        url: '/images/icons/react.svg',
-        title: 'React.js',
-      },
-      {
-        url: '/images/icons/bootstrap.svg',
-        title: 'Bootstrap',
-      },
-      {
-        url: '/images/icons/django-icon.svg',
-        title: 'Django',
-      },
-    ],
-    image: '/images/project/alzanders.jpg',
-    popupImage: '/images/project/alzanders-main.jpg',
   },
   {
     id: 7,
